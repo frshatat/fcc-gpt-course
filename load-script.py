4448e835db146e90a194937e1384c621e6f8f709
faris <farisshatat4@gmail.com>
2024-05-13 18:09:12 -0700

    fix preprocess script to  output split

--- load-script.py
+++ load-script.py
@@ -3,7 +3,9 @@ import lzma
 from tqdm import tqdm
 from multiprocessing import Pool, cpu_count
 import concurrent.futures
+import json
 from datasets import load_dataset
+import pandas as pd
 
 
 def xz_files_in_dir(directory):
@@ -19,63 +21,47 @@ def xz_files_in_dir(directory):
 ## if it is downloaded, load it
 ## save it to disk
 
-def loadDataSetIfNotExists(dataset_name):
-    if not os.path.exists('./data'):
-        dataset = load_dataset(dataset_name)
-        dataset["train"].save_to_disk("./data")
-    else:
-        _N_DATA_FILES = 21
-        _DATA_FILES = ["subsets/urlsf_subset{:02d}.tar".format(i) for i in range(_N_DATA_FILES)]
+def loadDataSeIfNotExist(dataset_name):
+    # Define the paths to the files
+    file_path_train = 'output_file_train.txt'
+    file_path_val = 'out_file_val.txt'
+    vocab_file = 'vocab.txt'
 
-        dataset = load_dataset(dataset_name, data_files = _DATA_FILES)
-    return dataset
+    # Load the dataset
+    dataset = load_dataset(dataset_name)
 
-dataset = loadDataSetIfNotExists("Skylion007/openwebtext")
-dataset = dataset["train"]
+    # Access the 'train' split of the dataset and select the first 1000 examples
+    train_dataset = dataset['train'].select(range(1000))
 
-# dataset = load_dataset("Skylion007/openwebtext")
-# dataset.save_to_disk("./data")
+    # Shuffle the dataset and split it into training and validation sets
+    train_dataset = train_dataset.shuffle(seed=42)
+    num_rows = len(train_dataset)
+    split_point = int(num_rows * 0.8)
+    train_data = train_dataset[:split_point]['text']
+    val_data = train_dataset[split_point:]['text']
 
-folder_path = "./data"
-output_file_train = "output_train.txt"
-out_file_val = "output_val.txt"
-vocab_file = "vocab.txt"
+    vocab = set()
 
-files = xz_files_in_dir(folder_path)
-
-total_files = len(files)
-
-#Calculate the split index
-split_index = int(total_files * 0.9) # 90% train, 10% val
-files_train = files[:split_index]
-files_val = files[split_index:]
-
-# Proces the files for training and validation seperatly
-vocab = set()
-
-# Process the training files
-with open(output_file_train, "w", encoding = "utf-8") as outfile:
-    for count, filename in tqdm(files_train, total = len(files_train)):
-        file_path = os.path.join(folder_path, filename)
-        with lzma.open(file_path, "rt", encoding = "utf-8") as infile:
-            text = infile.read()
-            outfile.write(text)
-            characters = set(text)
+    # Process the training data
+    with open(file_path_train, 'w', encoding='utf-8') as outfile:
+        for record in train_data:
+            outfile.write(record + '\n')
+            characters = set(record)
             vocab.update(characters)
 
-# Process the validation files
-with open(out_file_val, "w", encoding = "utf-8") as outfile:
-    for filename in tqdm(files_val, total = len(files_val)):
-        file_path = os.path.join(folder_path, filename)
-        with lzma.open(file_path, "rt", encoding = "utf-8") as infile:
-            text = infile.read()
-            outfile.write(text)
-            characters = set(text)
+    # Process the validation data
+    with open(file_path_val, 'w', encoding='utf-8') as outfile:
+        for record in val_data:
+            outfile.write(record + '\n')
+            characters = set(record)
             vocab.update(characters)
 
-# Write the vocabulatory to a vocab.txt
-with open(vocab_file, "w", encoding = "utf-8") as vfile:
+    # Write the vocabulary to a file
+    with open(vocab_file, 'w', encoding='utf-8') as vfile:
         for char in vocab:
-            vfile.write(char + "\n")
+            vfile.write(char + '\n')
+
+# Call the function
+loadDataSeIfNotExist("Skylion007/openwebtext")
 
             
